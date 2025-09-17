b5b02360abf3a4aeb242f4793845f5c45640a58e
feldib <beni.feldmayer@gmail.com>
2025-09-17 16:22:50 +0300

    fixed typos

--- migrations/02-seed.sql
+++ migrations/02-seed.sql
@@ -102,7 +102,7 @@ insert into users (
 SELECT * FROM users;
 
 insert into categories (cname)
-values ("Oil Paining");
+values ("Oil Painting");
 
 insert into categories (cname)
 values ("Sculpture");
@@ -167,7 +167,7 @@ insert into artwork_pictures(artwork_id, picture_path, is_thumbnail)
 values (4, "https://i2.wp.com/zet.gallery/blog/wp-content/uploads/2016/02/Vicent-Van-Gogh-Starry-Night-Famous-Oil-Paintings-www.shairart.com_.jpg?fit=1280%2C1014", false);
 
 insert into artworks (title, artist_name, price, quantity, descript, category_id)
-values ( "New Artwork", "Benedek Földes", 8, 6, "The artwist came up with the idea while reading a philopsophycal work by Jean-Paul Sartre.", 1);
+values ( "New Artwork", "Benedek Földes", 8, 6, "The artist came up with the idea while reading a philosophical work by Jean-Paul Sartre.", 1);
 
 insert into artwork_pictures(artwork_id, picture_path, is_thumbnail)
 values (5, "https://img.freepik.com/free-vector/hand-drawn-elephant-outline_23-2149268000.jpg", true);
@@ -181,7 +181,7 @@ insert into artwork_pictures(artwork_id, picture_path, is_thumbnail)
 values (6, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Musée_Rodin_1.jpg/330px-Musée_Rodin_1.jpg", true);
 
 insert into artworks (title, artist_name, price, quantity, descript, category_id)
-values ( "The Statue of Libertyr", "Bartholdi", 1000, 1, "
+values ( "The Statue of Liberty", "Bartholdi", 1000, 1, "
 The Statue of Liberty (Liberty Enlightening the World; French: La Liberté éclairant le monde) is a colossal neoclassical sculpture on Liberty Island in New York Harbor in New York City, in the United States. The copper statue, a gift from the people of France, was designed by French sculptor Frédéric Auguste Bartholdi and its metal framework was built by Gustave Eiffel. The statue was dedicated on October 28, 1886."
 , 2);
 
@@ -200,7 +200,7 @@ values (8,
 
 insert into artworks (title, artist_name, price, quantity, descript, category_id)
 values (  "Legend", "János Götz", 340, 5, "
-János Götz was one of the most taleted sculptors of his time. Despite loosing his life at a young age, he has contributed tremendously to Hungarian art. This beautiful creation is considered by some to be one of his best.
+János Götz was one of the most talented sculptors of his time. Despite losing his life at a young age, he has contributed tremendously to Hungarian art. This beautiful creation is considered by some to be one of his best.
 ", 2);
 
 insert into artwork_pictures(artwork_id, picture_path, is_thumbnail)
@@ -213,7 +213,7 @@ insert into artwork_pictures(artwork_id, picture_path, is_thumbnail)
 values (9, "https://i2.wp.com/zet.gallery/blog/wp-content/uploads/2016/02/Vicent-Van-Gogh-Starry-Night-Famous-Oil-Paintings-www.shairart.com_.jpg?fit=1280%2C1014", false);
 
 insert into artworks (title, artist_name, price, quantity, descript, category_id)
-values (  "János Götz", "Jonanna Götz", 120, 4, "
+values (  "János Götz", "Johanna Götz", 120, 4, "
 Johanna Götz, daughter of János Götz, and similarly to him a talented sculptor made this stunning sculpture in his memory.
 ", 2);
 
@@ -228,7 +228,7 @@ values (10, "https://i2.wp.com/zet.gallery/blog/wp-content/uploads/2016/02/Vicen
 
 insert into artworks (title, artist_name, price, quantity, descript, category_id)
 values (  "Ferenc Molnár", "Johanna Götz", 43, 17, "
-Franz Molnar was a Hungarian-born author stage director dramatist and poet widely regarded as Hungarys most celebrated and controversial playwright. His primary aim through his writing was to entertain by transforming his personal experiences into literary works of art. He never connected to any one literary movement. However he did utilize the precepts of naturalism Neo-Romanticism Expressionism and Freudian psychoanalytic theories but only as long as they suited his desires. By fusing the realistic narrative and stage tradition of Hungary with Western influences into a cosmopolitan amalgam Molnár emerged as a versatile artist whose style was uniquely his own. 
+Franz Molnar was a Hungarian-born author, stage director, dramatist and poet widely regarded as Hungary's most celebrated and controversial playwright. His primary aim through his writing was to entertain by transforming his personal experiences into literary works of art. He never connected to any one literary movement. However, he did utilize the precepts of naturalism, Neo-Romanticism, Expressionism and Freudian psychoanalytic theories but only as long as they suited his desires. By fusing the realistic narrative and stage tradition of Hungary with Western influences into a cosmopolitan amalgam, Molnár emerged as a versatile artist whose style was uniquely his own. 
 ", 2);
 
 insert into artwork_pictures(artwork_id, picture_path, is_thumbnail)
@@ -243,7 +243,7 @@ values (11, "https://i2.wp.com/zet.gallery/blog/wp-content/uploads/2016/02/Vicen
 
 insert into artworks (title, artist_name, price, quantity, descript, category_id)
 values (  "Self-portrait", "Károly Ferenczy", 43, 17, "
-Károly Ferenczy (February 8, 1862 – March 18 1917) was a Hungarian painter and leading member of the Nagybánya artists colony. He was among several artists who went to Munich for study in the late nineteenth century where he attended free classes by the Hungarian painter Simon Hollósy. Upon his return to Hungary Ferenczy helped found the artists colony in 1896 and became one of its major figures. Ferenczy is considered the father of Hungarian impressionism and post-impressionism and the founder of modern Hungarian painting.
+Károly Ferenczy (February 8, 1862 – March 18, 1917) was a Hungarian painter and leading member of the Nagybánya artists colony. He was among several artists who went to Munich for study in the late nineteenth century where he attended free classes by the Hungarian painter Simon Hollósy. Upon his return to Hungary, Ferenczy helped found the artists colony in 1896 and became one of its major figures. Ferenczy is considered the father of Hungarian impressionism and post-impressionism and the founder of modern Hungarian painting.
 ", 1);
 
 insert into artwork_pictures(artwork_id, picture_path, is_thumbnail)
@@ -277,7 +277,7 @@ insert into tags (tname)
 values ("Inspiring");
 
 insert into tags (tname)
-values ("Avant-guarde");
+values ("Avant-garde");
 
 insert into tags (tname)
 values ("Thought provoking");
@@ -363,7 +363,7 @@ insert into reviews (artwork_id, user_id, review_text, approved, title)
 values(
 	1,
     2,
-    "I simply love it! The colours, the athmosphere... I fell like I'm there!",
+    "I simply love it! The colours, the atmosphere... I feel like I'm there!",
     true,
     "Good!"
 );
